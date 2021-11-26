a9994eec15f7564fb43116b0723cdb67aadddcf1
ruch0401 <ruch0401@gmail.com>
2021-11-26 10:21:24 -0800

    Implemented accordion view for ticket details | Incorporated API to fetch count of tickets returned

--- app.js
+++ app.js
@@ -31,6 +31,26 @@ app.get("/tickets", async (req, res) => {
     });
 });
 
+app.get("/ticket/count", async (req, res) => {
+  const options = {
+    method: "GET",
+    uri: `https://zccruchitusc.zendesk.com/api/v2/tickets/count`,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Basic " + btoa(`${process.env.USERNAME}:${process.env.PASSWORD}`),
+    },
+  };
+
+  request(options)
+    .then(function (response) {
+      res.status(200).json(JSON.parse(response));
+    })
+    .catch(function (err) {
+      console.log("Something went wrong");
+    });
+});
+
 // How to we start listening to the server
 app.listen(3000, () => {
   console.log("Server is now up and running and listening on PORT 3000");
--- client.js
+++ client.js
@@ -1,25 +1,58 @@
 $(document).ready(function () {
-  localStorage.clear();
+  console.log("Ready!");
+
+  var ticket_count;
+
   $.ajax({
-    url: "http://localhost:3000/tickets",
+    url: "http://localhost:3000/ticket/count",
     method: "GET",
     headers: { "Content-Type": "application/json" },
   }).done(function (data) {
-    const tickets_data = data.tickets;
-    console.log(tickets_data);
-    for (ticket of tickets_data) {
-      buildDiv(ticket);
-    }
+    ticket_count = data.count.value;
+    console.log("Number of tickets returned ", ticket_count);
   });
-});
 
-function buildDiv(ticket) {
-  $(".ticket-container")
-    .append(`<div class="ticket-div"><h2>${ticket.subject}</h2>
-    
-    <p>${ticket.tags}</p>
-    <p>${ticket.created_at}</p>
-    <p>${ticket.updated_at}</p>
-    <p>${ticket.description}</p>
-  <a href="${ticket.url}"></a></div></br>`);
-}
+  $(".fetch-tickets").on("click", function (e) {
+    e.preventDefault();
+    localStorage.clear();
+    $.ajax({
+      url: "http://localhost:3000/tickets",
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }).done(function (data) {
+      ticket_details = data.tickets;
+      for (ticket of ticket_details) {
+        console.log(ticket);
+        buildDiv(ticket);
+      }
+    });
+  });
+
+  function buildDiv(ticket) {
+    $(".tickets-accordion-holder").append(
+      `
+      <button class="accordion" id="${ticket.id}">${ticket.subject}</button>
+    <div class="panel">
+      <p>${ticket.description}</p>
+      <p>Created at: ${ticket.created_at}</p>
+      <p>Updated at: ${ticket.updated_at}</p>
+      <p>Tags: ${ticket.tags}</p>
+    </div>
+      `
+    );
+    attachAccordion(ticket.id);
+  }
+
+  function attachAccordion(id) {
+    var acc = document.getElementById(`${id}`);
+    acc.addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+});
