593211d2ec749b2230e3557364d05bac97a69e56
ruch0401 <ruch0401@gmail.com>
2021-11-27 11:44:54 -0800

    Removed hardcoded ticket object

--- app.test.js
+++ app.test.js
@@ -2,17 +2,6 @@ const supertest = require("supertest");
 const app = require("./app");
 
 test("GET /tickets", async () => {
-  const ticket = {
-    id: 2,
-    created_at: "2021-11-21T05:48:11Z",
-    updated_at: "2021-11-21T05:48:11Z",
-    description:
-      "Aute ex sunt culpa ex ea esse sint cupidatat aliqua ex consequat sit reprehenderit. Velit labore proident quis culpa ad duis adipisicing laboris voluptate velit incididunt minim consequat nulla. Laboris adipisicing reprehenderit minim tempor officia ullamco occaecat ut laborum.\n\nAliquip velit adipisicing exercitation irure aliqua qui. Commodo eu laborum cillum nostrud eu. Mollit duis qui non ea deserunt est est et officia ut excepteur Lorem pariatur deserunt.",
-    priority: null,
-    status: "open",
-    tags: ["est", "incididunt", "nisi"],
-  };
-
   await supertest(app)
     .get("/tickets")
     .expect(200)
