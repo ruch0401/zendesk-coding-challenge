4b72e6bc04e02669e62b434f15f46d495dc6605c
ruch0401 <ruch0401@gmail.com>
2021-11-28 10:12:51 -0800

    Added new unit tests

--- app.test.js
+++ app.test.js
@@ -1,7 +1,7 @@
 const supertest = require("supertest");
 const app = require("./app");
 
-test("Tests whether /tickets returns the response where key=tags is an array of length 3", async () => {
+test("Tests whether /tickets returns the response which is an array", async () => {
   await supertest(app)
     .get("/tickets")
     .expect(200)
@@ -9,13 +9,29 @@ test("Tests whether /tickets returns the response where key=tags is an array of 
       var tickets = response.body.tickets;
       // Check type and length
       expect(Array.isArray(tickets)).toBeTruthy();
-      expect(tickets.length).toEqual(100);
+    });
+});
 
-      // Check data
+test("Tests whether /tickets returns the response where key=tags is an array of length 3", async () => {
+  await supertest(app)
+    .get("/tickets")
+    .expect(200)
+    .then((response) => {
+      var tickets = response.body.tickets;
       var i;
       for (i = 0; i < tickets.length; i++) {
         expect(Array.isArray(tickets[0].tags)).toBeTruthy();
         expect(tickets[0].tags.length).toBe(3);
       }
+    });
+});
+
+test("Tests whether the response from /tickets is paginated if count of tickets is greater than 25", async () => {
+  await supertest(app)
+    .get("/tickets")
+    .expect(200)
+    .then((response) => {
+      var tickets = response.body.tickets;
+      expect(tickets.length / 25).toBe(4);
     });
 });
